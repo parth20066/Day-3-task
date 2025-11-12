ec743bed631b7ba50d3f59847e3f9f0cd5829c80
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-12 05:29:04 -0800

    Create simple_interst.py

---
+++ simple_interst.py
@@ -0,0 +1,11 @@
+def simple_interst(p,r,t):
+  p=int(input("enter the principal amount "))
+  r=float(input("enter the rate" ))
+  t=int(input("enter the time" ))
+  simple_interst=(p*r*t)/100
+  return simple_interst
+simple_interst(p=1,r=2,t=3)
+enter the principal amount10
+enter the rate20
+enter the time30
+60.0

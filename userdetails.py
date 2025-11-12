59adb782c07d5d3a1e6663613af458a6fa4d5600
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-12 05:10:08 -0800

    Create userdetails.py

---
+++ userdetails.py
@@ -0,0 +1,12 @@
+def userdetails(a,b):
+  name=input("enter your name")
+  age=int(input("enter your age"))
+  roll_number=int(input("enter your rool_number"))
+  addition=age+roll_number
+  multiplication=a*b
+  return name , age,roll_number,addition , multiplication
+userdetails(3,5)
+enter your nameprarthana
+enter your age19
+enter your rool_number29
+('prarthana', 19, 29, 48, 15)

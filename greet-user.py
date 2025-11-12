0a109f324c9017132edb66f9356fa43448d06724
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-12 05:37:14 -0800

    Create greet-user.py

---
+++ greet-user.py
@@ -0,0 +1,8 @@
+def greet_user():
+  name=input("enter the name")
+  return f"Hello, {name}! Welcome to Python."
+greet_user()  
+def greet_user():
+  name=input("enter the name")
+  return f"Hello, {name}! Welcome to Python."
+greet_user()  

67ad85f805178304898faa4bdfb1c010a85855fb
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-12 05:31:46 -0800

    Create fahrenhit.py

---
+++ fahrenhit.py
@@ -0,0 +1,7 @@
+def fahrenhit_to_celsius(f):
+  f=int(input("enter the fahrenhit"))
+  fahrenhit_to_celsius=(f-32)*5/9
+  return fahrenhit_to_celsius
+fahrenhit_to_celsius(f=1)
+enter the fahrenhit27
+-2.7777777777777777
